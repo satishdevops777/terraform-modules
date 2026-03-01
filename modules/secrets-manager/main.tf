b77f0b3ec9eb0bcf1d22d40865bf8e96106cc2bc
SATISH REDDY SATTI <satishdevops3789@gmail.com>
2026-03-01 13:55:50 +0530

    Update main.tf

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -6,3 +6,19 @@ resource "aws_secretsmanager_secret" "this" {
 
   tags = var.tags
 }
+
+resource "aws_secretsmanager_secret_version" "this" {
+  secret_id     = aws_secretsmanager_secret.this.id
+  secret_string = var.secret_string
+}
+
+resource "aws_secretsmanager_secret_rotation" "this" {
+  count = var.enable_rotation ? 1 : 0
+
+  secret_id           = aws_secretsmanager_secret.this.id
+  rotation_lambda_arn = var.rotation_lambda_arn
+
+  rotation_rules {
+    automatically_after_days = var.rotation_days
+  }
+}
