bdb4be0cd9c1a157eb9908242670a8d33691eb86
SATISH REDDY SATTI <satishdevops3789@gmail.com>
2026-03-01 13:54:46 +0530

    Create main.tf

---
+++ modules/secrets-manager/main.tf
@@ -0,0 +1,8 @@
+resource "aws_secretsmanager_secret" "this" {
+  name                    = var.name
+  description             = var.description
+  kms_key_id              = var.kms_key_id
+  recovery_window_in_days = var.recovery_window_in_days
+
+  tags = var.tags
+}
