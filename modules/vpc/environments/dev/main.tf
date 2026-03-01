5ac1448d7758137d58bc2427cb25467d30d6e9c5
SATISH REDDY SATTI <satishdevops3789@gmail.com>
2026-03-01 13:52:45 +0530

    Create main.tf

---
+++ modules/vpc/environments/dev/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name = "dev"
+
+  cidr_block = "10.0.0.0/16"
+
+  availability_zones = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+
+  tags = {
+    Environment = "dev"
+    Owner       = "satish"
+  }
+}
